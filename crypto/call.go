c38a8dfe5688c69e325fa71edc6698d00a1c50a4
FG420 <facchinatog97@gmail.com>
2024-08-14 11:34:56 +0200

    donezo

--- crypto/call.go
+++ crypto/call.go
@@ -2,52 +2,185 @@ package crypto
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"os"
+	"os/exec"
+	"slices"
+	"strings"
 	"time"
 
 	"github.com/FG420/crypto-tracker/types"
 )
 
 var (
-	baseApi  = "https://api.kucoin.com/api/v1/market/stats?symbol="
-	baseApi2 = "-USDT"
+	baseApi    = "https://api.kucoin.com/api/v1/market/stats?symbol="
+	baseApi2   = "-USDT"
+	coinsNames = []string{"BTC", "DOGE", "SOL", "ETH"}
 )
 
-func CallApi(coinName string) *types.Coin {
+func callApi(coinName string) *types.Coin {
 	apiUrl := baseApi + coinName + baseApi2
 	res, err := http.Get(apiUrl)
 	if err != nil {
-		log.Panicf("Error during API call for %s: %v", coinName, err)
+		log.Printf("Error during API call for %s: %v", coinName, err)
 
 	}
 
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
-		log.Panicf("Error during body reading for %s: %v", coinName, err)
+		log.Printf("Error during body reading for %s: %v", coinName, err)
 		res.Body.Close()
 	}
 
 	var coin types.Coin
 	if err := json.Unmarshal(body, &coin); err != nil {
-		log.Panicf("Error during unmarshal result for %s: %v", coinName, err)
-	}
-
-	if coin.Data.Buy == "" {
-		log.Panic("Error! The data for this crypto coin is not possible to retrive because the coin doesn't exist!")
-
+		log.Printf("Error during unmarshal result for %s: %v", coinName, err)
 	}
 
 	return &coin
 }
 
-func GetData(coinName string) {
-	coin := CallApi(coinName)
+func LoadCoins() (types.Coins, error) {
+	var listCoins types.Coins
+	readJson, err := os.ReadFile("coins.json")
+	if err != nil {
+		log.Print("File not found, creating a new one.")
+		listCoins.Names = coinsNames
+		if err := SaveCoins(listCoins); err != nil {
+			return listCoins, fmt.Errorf("failed to create file: %v", err)
+		}
+		log.Print("File created")
+	} else if err := json.Unmarshal(readJson, &listCoins); err != nil {
+		return listCoins, fmt.Errorf("error during unmarshal: %v", err)
+	}
+	return listCoins, nil
+}
+
+func SaveCoins(listCoins types.Coins) error {
+	content, err := json.Marshal(listCoins)
+	if err != nil {
+		return fmt.Errorf("failed to marshal coins: %v", err)
+	}
+	if err := os.WriteFile("coins.json", content, 0644); err != nil {
+		return fmt.Errorf("failed to write coins to file: %v", err)
+	}
+	return nil
+}
+
+func FollowCryptoMarket() {
+	coinName := inputName()
+	fmt.Print("\nPress the backspace key to terminate this action")
+
+	exec.Command("stty", "-F", "/dev/tty", "-echo", "cbreak", "min", "1").Run()
+	defer exec.Command("stty", "-F", "/dev/tty", "echo", "-cbreak").Run()
+
+	var b []byte = make([]byte, 1)
+	stopChan := make(chan bool)
+
+	go func() {
+		for {
+			select {
+			case <-stopChan:
+				return
+			default:
+				getData(coinName)
+				time.Sleep(3 * time.Second)
+			}
+		}
+	}()
+
+	for {
+		_, err := os.Stdin.Read(b)
+		if err != nil {
+			log.Fatalf("Error reading from stdin: %v", err)
+		}
+
+		if b[0] == 127 {
+			fmt.Print("\nAction terminated!")
+			stopChan <- true
+			break
+		}
+	}
+}
+
+func AddNewCrypto(listCoins *types.Coins) {
+	coinName := inputName()
+	coin := callApi(coinName)
+	if coin.Data.Buy == "" {
+		log.Print("Error! The data for this crypto coin cannot be retrieved because the coin doesn't exist!")
+		return
+	}
+
+	if slices.Contains(listCoins.Names, coinName) {
+		log.Print("The file already contains the coin inputted")
+		log.Print(listCoins)
+		return
+	}
+
+	listCoins.Names = append(listCoins.Names, coinName)
+	if err := SaveCoins(*listCoins); err != nil {
+		log.Printf("Failed to add coin: %v", err)
+	} else {
+		log.Printf("Coin added: %s", coinName)
+	}
+}
+
+func DeleteCrypto(listCoins *types.Coins) {
+	coinName := inputName()
+	if !slices.Contains(listCoins.Names, coinName) {
+		log.Print("The file doesn't contain the coin inputted")
+		log.Print(listCoins)
+		return
+	}
+
+	index := slices.Index(listCoins.Names, coinName)
+	listCoins.Names = slices.Delete(listCoins.Names, index, index+1)
+
+	if err := SaveCoins(*listCoins); err != nil {
+		log.Printf("Failed to delete coin: %v", err)
+	} else {
+		log.Printf("Coin deleted: %s", coinName)
+	}
+}
+
+func SaveCryptoStock(listCoins types.Coins) {
+	filename := "crypto-stock.json"
+	var arrayCoins []types.Coin
+
+	for _, coin := range listCoins.Names {
+		coins := callApi(coin)
+		arrayCoins = append(arrayCoins, *coins)
+	}
+
+	content, err := json.Marshal(arrayCoins)
+	if err != nil {
+		log.Print("Something went wrong during marshalling!")
+		return
+	}
+
+	if err := os.WriteFile(filename, content, 0644); err != nil {
+		log.Print("Something went wrong during file writing!")
+	} else {
+		fmt.Printf("\nFile created successfully!")
+	}
+}
+
+func inputName() string {
+	fmt.Print("\nInsert a coin symbol: ")
+	var inputName string
+	fmt.Scan(&inputName)
+	return strings.ToUpper(inputName)
+}
+
+func getData(coinName string) {
+	coin := callApi(coinName)
 
 	time := time.Unix(coin.Data.Time, 0)
 
-	log.Printf("\n                RESULT"+
+	fmt.Printf("\n                RESULT"+
 		"\n____________________________________"+
 		"\n- Searched Coin: %s"+
 		"\n- Average Price: %s"+
@@ -65,7 +198,7 @@ func GetData(coinName string) {
 		"\n- Volume: %s"+
 		"\n- Volume Value: %s"+
 		"\n- Time: %s"+
-		"\n____________________________________",
+		"\n____________________________________\n",
 		coin.Data.Symbol,
 		coin.Data.AveragePrice,
 		coin.Data.Last,
--- main.go
+++ main.go
@@ -1,111 +1,47 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
-	"os"
-	"slices"
-	"strings"
 
 	"github.com/FG420/crypto-tracker/crypto"
-	"github.com/FG420/crypto-tracker/types"
 )
 
 func main() {
+	log.Print("WELCOME!")
 
-	fmt.Print("WELCOME!")
-
-	var listCoins types.Coins
-	readJson, err := os.ReadFile("coins.json")
-	if err != nil {
-		log.Print("File not found")
-		os.Create("coins.json")
-		log.Print("File created")
-	}
-
-	if err := json.Unmarshal(readJson, &listCoins); err != nil {
-		log.Printf("Error during unmarshal file for %s: %v", readJson, err)
-		return
-	}
-
-	fmt.Printf("\n\nThese are the Crypto currencies currently saved: \n%s\n\nWhat do you wanna do?\n"+
-		"[1] - Follow the inputed crypto currency market.\n"+
-		"[2] - Insert a new crypto currency to follow in the file.\n"+
-		"[3] - Delete the inputed crypto currency from the file.\n", listCoins.Names)
-
-	input := ""
-	// fmt.Print("Insert a coin symbol: ")
-	fmt.Print("Select a choice: ")
-	fmt.Scan(&input)
-
-	var listCoins2 types.Coins
-
-	switch input {
-	case "1":
-		coinName := inputName()
-		crypto.GetData(coinName)
-
-	case "2":
-		coinName := inputName()
-		crypto.CallApi(coinName)
-
-		if slices.Contains(listCoins.Names, coinName) {
-			log.Print("The file contains the coin inputed")
-			log.Print(listCoins)
-			return
-		}
-
-		addCoin := append(listCoins.Names, coinName)
-		listCoins2.Names = addCoin
-
-		content, err := json.Marshal(listCoins2)
+	for {
+		listCoins, err := crypto.LoadCoins()
 		if err != nil {
-			fmt.Println(err)
+			log.Print(err)
+			continue
 		}
 
-		if err := os.WriteFile("coins.json", []byte(content), os.ModeAppend); err != nil {
-			log.Print("Data not inserted")
+		fmt.Printf("\n\nThese are the Crypto currencies currently saved: \n%s\n\nWhat do you want to do?\n"+
+			"[1] - Follow the inputted crypto currency market.\n"+
+			"[2] - Insert a new crypto currency to follow in the file.\n"+
+			"[3] - Delete the inputted crypto currency from the file.\n"+
+			"[4] - Print in a new .json file the current stock market for all the cryptos saved in the .json file.\n"+
+			"[0] - Exit.\n", listCoins.Names)
+
+		input := ""
+		fmt.Print("Select a choice: ")
+		fmt.Scan(&input)
+
+		switch input {
+		case "1":
+			crypto.FollowCryptoMarket()
+		case "2":
+			crypto.AddNewCrypto(&listCoins)
+		case "3":
+			crypto.DeleteCrypto(&listCoins)
+		case "4":
+			crypto.SaveCryptoStock(listCoins)
+		case "0":
+			fmt.Println("Exiting...")
 			return
+		default:
+			log.Print("Select one of the choices!")
 		}
-
-		log.Print(listCoins2)
-
-	case "3":
-		coinName := inputName()
-		if !slices.Contains(listCoins.Names, coinName) {
-			log.Print("The file doesn't contain the coin inputed")
-			log.Print(listCoins)
-			return
-		}
-
-		index := slices.Index(listCoins.Names, coinName)
-		del := slices.Delete(listCoins.Names, index, index+1)
-
-		listCoins2.Names = del
-
-		content, err := json.Marshal(listCoins2)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		if err := os.WriteFile("coins.json", []byte(content), os.ModeAppend); err != nil {
-			log.Print("Data not inserted")
-			return
-		}
-
-		log.Print(listCoins2)
-
-	default:
-		log.Print("Select one of the three choises!")
 	}
-
-}
-
-func inputName() string {
-	inputName := ""
-	fmt.Print("Insert a coin symbol: ")
-	fmt.Scan(&inputName)
-	coinName := strings.ToUpper(inputName)
-	return coinName
 }
